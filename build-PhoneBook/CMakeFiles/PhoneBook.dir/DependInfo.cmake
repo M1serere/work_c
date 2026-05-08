4895c062f5b1505ff6a76602a6782c5709bc6370
M1serere <1012rasserta1012@gmail.com>
2026-05-08 23:15:52 +1000

    файл exe кривоват

---
+++ build-PhoneBook/CMakeFiles/PhoneBook.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "PhoneBook_autogen/timestamp" "custom" "PhoneBook_autogen/deps"
+  "C:/Users/croco/Desktop/nom2/build-PhoneBook/PhoneBook_autogen/mocs_compilation.cpp" "CMakeFiles/PhoneBook.dir/PhoneBook_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/PhoneBook.dir/PhoneBook_autogen/mocs_compilation.cpp.obj.d"
+  "C:/Users/croco/Desktop/nom2/PhoneBook/database.cpp" "CMakeFiles/PhoneBook.dir/database.cpp.obj" "gcc" "CMakeFiles/PhoneBook.dir/database.cpp.obj.d"
+  "C:/Users/croco/Desktop/nom2/PhoneBook/main.cpp" "CMakeFiles/PhoneBook.dir/main.cpp.obj" "gcc" "CMakeFiles/PhoneBook.dir/main.cpp.obj.d"
+  "C:/Users/croco/Desktop/nom2/PhoneBook/mainwindow.cpp" "CMakeFiles/PhoneBook.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/PhoneBook.dir/mainwindow.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-PhoneBook/CMakeFiles/PhoneBook.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhoneBook_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/PhoneBook_autogen.dir/ParseCache.txt"
+  "PhoneBook_autogen"
+  "CMakeFiles/PhoneBook.dir/PhoneBook_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/PhoneBook.dir/PhoneBook_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/PhoneBook.dir/database.cpp.obj"
+  "CMakeFiles/PhoneBook.dir/database.cpp.obj.d"
+  "CMakeFiles/PhoneBook.dir/main.cpp.obj"
+  "CMakeFiles/PhoneBook.dir/main.cpp.obj.d"
+  "CMakeFiles/PhoneBook.dir/mainwindow.cpp.obj"
+  "CMakeFiles/PhoneBook.dir/mainwindow.cpp.obj.d"
+  "PhoneBook.exe"
+  "PhoneBook.exe.manifest"
+  "PhoneBook.pdb"
+  "PhoneBook_autogen/mocs_compilation.cpp"
+  "PhoneBook_autogen/timestamp"
+  "libPhoneBook.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhoneBook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
